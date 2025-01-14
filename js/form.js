685afd5d93623726d4cdf94482982056fa9077a3
dlmarques <daniel.marquesedigital@gmail.com>
2025-01-14 11:07:38 +0000

    some reponsive fixes

--- js/form.js
+++ js/form.js
@@ -6,8 +6,8 @@ guestsNumberInput.addEventListener("change", (e) => {
   guestsDiv.innerHTML = "";
   for (let i = 0; i < number; i++) {
     guestsDiv.innerHTML += `
-    <h2>Convidado ${i + 1}</h2>
-      <div style="padding-bottom: 8px; display: flex; flex-direction: column; gap: 8px;">
+    <h2 style="padding-top: 8px;">Convidado ${i + 1}</h2>
+      <div style="padding-bottom: 8px; display: flex; flex-direction: column; gap: 8px; align-items: center; width:100%">
         <input type="text"  id="guest-name-${i + 1}" name="guest-name-${
       i + 1
     }" placeholder="Nome" required>
